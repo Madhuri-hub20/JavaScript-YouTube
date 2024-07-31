b24dab39ebc7bcef5617f0203586adffc5db5761
Madhuri-hub20 <60954434+Madhuri-hub20@users.noreply.github.com>
2024-07-31 16:16:00 +0000

    numbers and maths opeaions

---
+++ 01_Basics/nums_and_math_06.js
@@ -0,0 +1,14 @@
+const score = 400;
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+//**************Math******************
+
+console.log(Math);
+console.log(Math.abs(-4));//- value get positive or vice versa
+console.log(Math.round(4.2));//round of value
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
