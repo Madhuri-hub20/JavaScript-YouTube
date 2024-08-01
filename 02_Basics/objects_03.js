7b7bb443f434e28ea2e961e91fd1cb455c93da35
Madhuri-hub20 <60954434+Madhuri-hub20@users.noreply.github.com>
2024-08-01 07:17:23 +0000

    objects info

---
+++ 02_Basics/objects_03.js
@@ -0,0 +1,17 @@
+//signleton
+//object Literals
+
+const mySymbol = Symbol("myKey");
+
+const myObj = {
+    name : "Madhuri",
+    age : 24,
+    [mySymbol] : "key",
+    location : "Pune",
+    email : "mdh202@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", " Saturday"] 
+}
+
+console.log(myObj);
+console.log(myObj[mySymbol]);
