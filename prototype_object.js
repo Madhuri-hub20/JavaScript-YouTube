099e5e7b823b5f623214c8e07a88d31d6ceaa53d
Madhuri-hub20 <60954434+Madhuri-hub20@users.noreply.github.com>
2024-08-07 05:15:46 +0000

    prototype

--- prototype_object.js
+++ prototype_object.js
@@ -1,6 +1,7 @@
 let myName = "Madhuri    ";
 
 String.prototype.trueLength = function(){
+    //String chya jagevr aapn oject pn gheu shakto..bcoz object parent aahe string ,array,function  cha
     console.log(`${this}`);
     console.log(`This is Length ${this.trim().length}`);
 }
